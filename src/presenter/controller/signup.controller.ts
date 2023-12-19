7bddc6867ca3b880ab36b25380ee6dab31a5bf8a
cledson leite <csbetsonline@gmail.com>
2023-12-19 13:48:24 -0300

    feat: ensure SignUpController returns missing params error  if no name provided

--- src/presenter/controller/signup.controller.ts
+++ src/presenter/controller/signup.controller.ts
@@ -2,7 +2,8 @@ export class SignUpController {
     handle(request) {
         request
         return {
-            statusCode: 400
+            statusCode: 400,
+            body: new Error('Missing Params Error')
         }
     }
 }
